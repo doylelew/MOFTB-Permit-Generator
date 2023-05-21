509b852337fd735cff2b677d2dee14a3542c9214
doylelew <69176020+doylelew@users.noreply.github.com>
2023-05-21 12:41:08 -0400

    Fix the app info

--- main.py
+++ main.py
@@ -9,20 +9,27 @@ from WebDriver import OpenBrowser, Login, ProjectList, Jump_To_URL, PermitList
 # app info
 #################################################################
 """
+    MOFTB NYC Permit Generator
     An app that submits permits for filming television and movies in the City of New York to bypass having to use the MOFTB website itself.
     The Mayor's Office website often times out not saving your work which requires you to start over which this app bypasses that by saving locally before sending the info over to the MOFTB site.
     The Mayor's Office also will at times make changes from your initial request on the permit without informing you which can be hard to verify without a copy of what you submitted. 
     This will save a plain text version of what you submitted so you can more easily compare your final permit with what you submitted to the Mayor's office.
    
-    Name: "MOFTB NYC Permit Generator"
-    Version: "0.1.0"
-    Author: "Doyle Lewis"
-    Email: "odoylerules@pennyworth.network"
+    Author: Doyle Lewis
+    Email: odoylerules@pennyworth.network
+    
+    Version: 0.1.0
+    Status: Development   
 
 """
-__version__ = "0.1.0"
+
+
 __author__ = "Doyle Lewis"
+__credits__ = ["Doyle Lewis"]
+__version__ = "0.1.0"
+__maintainer__ = "Doyle Lewis"
 __email__ = "odoylerules@pennyworth.network"
+__status__ = "Development"
 
 #################################################################
 # Constants
